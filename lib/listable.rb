6fd8fbb56f3d69e6055e8070bc231b492c87bd11
Alejandro Serradilla <alexserradillarufino@gmail.com>
2016-04-14 16:07:10 +0200

    Moved formt_date and formt_priority into Listable module

--- lib/listable.rb
+++ lib/listable.rb
@@ -1,5 +1,29 @@
 module Listable
+        # Formats the description
         def format_description(description)
                 "#{description}".ljust(30)
+        end
+
+        # Formats the date
+        def format_date(options={})
+                # If its a todo item
+                if options[:start_date]
+                        dates = options[:start_date].strftime("%D") if options[:start_date]
+                        dates << " -- " + options[:end_date].strftime("%D") if options[:end_date]
+                        dates = "N/A" if !dates
+                        return dates
+                else
+                        options[:end_date] ? options[:end_date].strftime("%D") : "No due date"
+                end
+        end
+
+        # Formats the priority
+        def format_priority(priority)
+                # If its a todo item
+                value = " ⇧" if priority == "high"
+                value = " ⇨" if priority == "medium"
+                value = " ⇩" if priority == "low"
+                value = "" if !priority
+                return value
         end
 end
