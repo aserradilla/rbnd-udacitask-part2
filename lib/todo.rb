2a7270523eed3e0f54faaf9b49cc90a78bc3b784
Alejandro Serradilla <alexserradillarufino@gmail.com>
2016-04-15 12:30:37 +0200

    Added methods for handling errors

--- lib/todo.rb
+++ lib/todo.rb
@@ -1,15 +1,26 @@
 class TodoItem
-  include Listable
-  attr_reader :description, :due, :priority
+        include Listable
+        attr_reader :description, :due, :priority
 
-  def initialize(description, options={})
-    @description = description
-    @due = options[:due] ? Date.parse(options[:due]) : options[:due]
-    @priority = options[:priority]
-  end
-  def details
-    format_description(@description) + "due: " +
-    format_date(end_date: @due) +
-    format_priority(@priority)
-  end
+        def initialize(description, options={})
+                @description = description
+                @due = options[:due] ? Chronic.parse(options[:due]) : options[:due]
+                check_priority(options[:priority]) if options[:priority]
+                @priority = options[:priority]
+        end
+
+        def details
+                format_description(@description) + "due: " +
+                format_date(end_date: @due) +
+                format_priority(@priority)
+        end
+
+        private
+
+        # Raises error when priority is unknown
+        def check_priority(priority)
+                if priority != "low" && priority != "high" && priority != "medium"
+                        raise UdaciListErrors::InvalidPriorityValue, "'#{priority}' is not a valid priority."
+                end
+        end
 end
--- lib/udacilist.rb
+++ lib/udacilist.rb
@@ -1,25 +1,44 @@
-class UdaciList
-  attr_reader :title, :items
+ class UdaciList
+         attr_reader :title, :items
 
-  def initialize(options={})
-    @title = options[:title]
-    @items = []
-  end
-  def add(type, description, options={})
-    type = type.downcase
-    @items.push TodoItem.new(description, options) if type == "todo"
-    @items.push EventItem.new(description, options) if type == "event"
-    @items.push LinkItem.new(description, options) if type == "link"
-  end
-  def delete(index)
-    @items.delete_at(index - 1)
-  end
-  def all
-    puts "-" * @title.length
-    puts @title
-    puts "-" * @title.length
-    @items.each_with_index do |item, position|
-      puts "#{position + 1}) #{item.details}"
-    end
-  end
+         def initialize(options={})
+                 @title = options[:title]
+                 @items = []
+         end
+
+         def add(type, description, options={})
+                 type = type.downcase
+                 check_type(type)
+                 @items.push TodoItem.new(description, options) if type == "todo"
+                 @items.push EventItem.new(description, options) if type == "event"
+                 @items.push LinkItem.new(description, options) if type == "link"
+         end
+
+         def delete(index)
+                 check_index(index-1)
+                 @items.delete_at(index - 1)
+         end
+
+         def all
+                 puts "-" * @title.length
+                 puts @title
+                 puts "-" * @title.length
+                 @items.each_with_index do |item, position|
+                         puts "#{position + 1}) #{item.details}"
+                 end
+         end
+
+         private
+
+         # Raises error when type is unknown
+         def check_type(type)
+                 if type != "todo" && type != "event" && type != "link"
+                         raise UdaciListErrors::InvalidItemType, "type '#{type}' is not supported."
+                 end
+         end
+
+         # Raises error when index exceeds array's length
+         def check_index(index)
+                 raise UdaciListErrors::IndexExceedsListSize, "There is no such item to delete, '#{index}' is not valid" if index > @items.length
+         end
 end
